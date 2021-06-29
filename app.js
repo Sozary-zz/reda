5613326bf699a9bbb48b8d34ef1834e3bbb5f898
Mehdi Ayache <mehdiayache@hotmail.fr>
2021-06-29 15:34:06 +0200

    :lipstick: appearence

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
+const container = document.querySelector(".container");
 
-const container = document.querySelector('.row')
-
-function createCard(){
-    container.innerHTML = recipes.map(recette =>
+function createCard() {
+  container.innerHTML = recipes
+    .map(
+      (recette) =>
         `
-        <div class="card col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-0 col-lg-4 " style="width: 18rem;">
+        <div class="card mx-2" style="width: 32%; ">
             <div class="card-img"></div>
-            <div class="card-body">
+            <div class="card-body" >
             <header class="card-title">
                 <h2> ${recette.name} </h2>
                 <span><i class="fas fa-clock" ></i> ${recette.time} min</span>
             </header>
-                <div class="card-ingredient">
-                    ${recette.ingredients.map(element =>
-                         `<div><span><b>${element.ingredient}: </b></span>
-                         <span>${ "quantity" in element ? element.quantity : ""} </span>
-                         <span>${ "unit" in element? element.unit : ""}</span>
-                         </div>`).join('')}
+                <div class="card-ingredient " >
+                    ${recette.ingredients
+                      .map(
+                        (element) =>
+                          `<div><span><b>${element.ingredient} </b></span>
+                         <span>${
+                           "quantity" in element ? ": " + element.quantity : ""
+                         } </span>
+                         <span>${
+                           "quantite" in element ? ": " + element.quantite : ""
+                         } </span>
+                         <span>${"unit" in element ? element.unit : ""}</span>
+                         </div>`
+                      )
+                      .join("")}
                 </div>
-                <div class="card-text">
+                <div class="card-text" >
                     <span>${recette.description}</span>
                 </div>        
             </div>
         </div>
-        `).join('')
+        `
+    )
+    .join("");
 }
-createCard()
+createCard();

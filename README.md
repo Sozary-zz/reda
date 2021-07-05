3201e796732a854395d8cc8e02fee24796ad4afe
Mehdi Ayache <mehdiayache@hotmail.fr>
2021-07-05 17:10:07 +0200

    :hammer: first algo

---
+++ README.md
@@ -0,0 +1,2 @@
+# KODADReda_07_21-06-2021
+Projet numéro 7  : Utilisation de bootstrap et des algorithmes de recherche JS
--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-const container = document.querySelector(".container");
+const container = document.querySelector(".row");
 
 function createCard() {
   container.innerHTML = recipes
     .map(
       (recette) =>
         `
-        <div class="card mx-2" style="width: 32%; ">
+        <div class="card col-sm-12 col-md-offset-2 col-md-8 col-lg-offset-0 col-lg-4 " style="width: 18rem;">
             <div class="card-img"></div>
-            <div class="card-body" >
+            <div class="card-body">
             <header class="card-title">
                 <h2> ${recette.name} </h2>
                 <span><i class="fas fa-clock" ></i> ${recette.time} min</span>
             </header>
-                <div class="card-ingredient " >
+                <div class="card-ingredient">
                     ${recette.ingredients
                       .map(
                         (element) =>
-                          `<div><span><b>${element.ingredient} </b></span>
+                          `<div><span><b>${element.ingredient}: </b></span>
                          <span>${
-                           "quantity" in element ? ": " + element.quantity : ""
-                         } </span>
-                         <span>${
-                           "quantite" in element ? ": " + element.quantite : ""
+                           "quantity" in element ? element.quantity : ""
                          } </span>
                          <span>${"unit" in element ? element.unit : ""}</span>
                          </div>`
                       )
                       .join("")}
                 </div>
-                <div class="card-text" >
+                <div class="card-text">
                     <span>${recette.description}</span>
                 </div>        
             </div>
@@ -38,3 +35,14 @@ function createCard() {
     .join("");
 }
 createCard();
+
+function algoRecherche(combinaisons, text) {
+  let result = {};
+  for (let combinaison of combinaisons) {
+    result[combinaison] = recipes.filter((recipe) => {
+      recipe[combinaison].toLowerCase().includes(recipe[text]).toLowerCase();
+    });
+  }
+
+  return result;
+}
